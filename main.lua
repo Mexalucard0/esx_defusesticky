34578edc7fec13ccfd9def42775178f49b5ec06a
Yavuz Selim <57921834+kryp-x@users.noreply.github.com>
2020-02-25 15:40:23 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,33 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+	end
+
+	PlayerData = ESX.GetPlayerData()
+end)
+
+local veh = GetClosestVehicle(pos.x,pos.y,pos.z,100.00,0)
+ 
+local checkbomb = HasVehicleGotProjectileAttached(GetPlayerPed(-1),veh,get_hash_key("weapon_stickybomb") ,-1)
+	
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+			if IsEntityAVehicle(veh) then
+				if checkbomb then
+					DrawText3D(pos.x,pos.y,pos.z, "Bombayi cözmek icin [E] basiniz")
+					if IsControlJustReleased(0, 38) then
+						RemoveAllStickyBombsFromEntity(veh)
+						
+					end
+				end
+			end
+	end
+end)
